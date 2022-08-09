f9afac29b1ee5a572565714cb570a5bb3e0bd30d
saeedkhal <nnowyan@gmail.com>
2022-08-09 21:55:11 +0200

    modify footer

--- src/components/sharedCompnents/Footer.js
+++ src/components/sharedCompnents/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import logo from '../../assets/images/SALogoFillBlack.svg';
 function Footer(props) {
   return (
     <footer className="home-footer">
-      © 2022 <span>Saeed Khaled</span> All rights reserved
+      <div>
+        © 2022 Copyright <img className="logo-creator" src={logo} alt="logo" />
+        All Rights Reserved
+      </div>
+      <div>
+        Designed by <span>Saeed Khaled</span>
+      </div>
     </footer>
   );
 }
