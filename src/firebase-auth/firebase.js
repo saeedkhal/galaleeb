f13d38bdcf7fe71c3975d3c3c0c20e0e45d3ddec
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-02-05 01:49:43 +0200

    sovle env var

--- src/firebase-auth/firebase.js
+++ src/firebase-auth/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.RAECT_APP_Firebase_apiKey,
-    authDomain: process.env.RAECT_APP_Firebase_authDomain,
-    projectId: process.env.RAECT_APP_Firebase_projectId,
-    storageBucket: process.env.RAECT_APP_Firebase_storageBucket,
-    messagingSenderId: process.env.RAECT_APP_Firebase_messagingSenderId,
-    appId: process.env.RAECT_APP_Firebase_appId
+    apiKey: process.env.REACT_APP_Firebase_apiKey,
+    authDomain: process.env.REACT_APP_Firebase_authDomain,
+    projectId: process.env.REACT_APP_Firebase_projectId,
+    storageBucket: process.env.REACT_APP_Firebase_storageBucket,
+    messagingSenderId: process.env.REACT_APP_Firebase_messagingSenderId,
+    appId: process.env.REACT_APP_Firebase_appId
 };
 
 const app = initializeApp(firebaseConfig);
