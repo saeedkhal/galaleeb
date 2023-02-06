30fe77848547a4f3c455bc0faf7ce2f179321919
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-02-06 15:54:41 +0200

    sovle side basr

--- src/components/sharedCompnents/SideBar.js
+++ src/components/sharedCompnents/SideBar.js
@@ -5,13 +5,13 @@ import { BsCartFill, BsFillPersonPlusFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import { useContextProvider } from '../../context/context';
 function SideBar() {
-  const { cart, CloseSideBar, isSidebarOpen } = useContextProvider();
+  const { cart, CloseSideBar, isSidebarOpen, user, signInwithGoogle, logOut } = useContextProvider();
   const cartTotal = cart.reduce((prev, current) => prev + current.quantity, 0) || 0
   return (
     <main className={isSidebarOpen ? 'sidebar oprnsidebare' : 'sidebar'}>
       <div className="sidebar-container">
         <section className="logo-x">
-          <Link to="/">
+          <Link to="/" onClick={() => CloseSideBar()} >
             <article className="logo-container">
               <span>GALALEEB</span>
               <img src={logo} alt="logo" className="logo" />
@@ -22,26 +22,33 @@ function SideBar() {
         </section>
         <ul className="navbar-routs-sidebar">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={() => CloseSideBar()} >Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" onClick={() => CloseSideBar()} >About</Link>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <Link to="/products" onClick={() => CloseSideBar()} >Products</Link>
           </li>
         </ul>
         <div className="cart-login-sidebar">
           <article className="cart-sidebar ">
-            <a href="/cart">
+            <Link to="/cart" onClick={() => CloseSideBar()} >
               cart <BsCartFill />
               <span className="cart-amount">{cartTotal}</span>
-            </a>
+            </Link>
           </article>
           <article className="login-sidebar">
-            <a href="/login">
-              login <BsFillPersonPlusFill />
-            </a>
+
+            {
+              !Object.keys(user).length ?
+                <span onClick={() => { signInwithGoogle(); CloseSideBar() }} >
+                  login
+                  <BsFillPersonPlusFill />
+                </span>
+                :
+                <span onClick={() => { logOut(); CloseSideBar() }} className="login">  LogOut <img src={user?.photoURL} alt='user' />  </span>
+            }
           </article>
         </div>
       </div>
