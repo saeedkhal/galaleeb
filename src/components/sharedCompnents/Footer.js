22d8cc007a08d76d2abd2b66c4ec593a0e531f08
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2024-05-20 02:38:58 +0300

    finish

--- src/components/sharedCompnents/Footer.js
+++ src/components/sharedCompnents/Footer.js
@@ -4,11 +4,14 @@ function Footer(props) {
   return (
     <footer className="home-footer">
       <div>
-        © 2022 Copyright <img className="logo-creator" src={logo} alt="logo" />
+        © {new Date().getFullYear()} Copyright <img className="logo-creator" src={logo} alt="logo" />
         All Rights Reserved
       </div>
       <div>
-        Designed by <span>Saeed Khaled</span>
+        Designed by 
+          <a target='_blank' href='https://saeedkhal.github.io/saeed-khaled-portfolio/index.html' rel="noreferrer">
+            Saeed Khaled
+          </a>
       </div>
     </footer>
   );
